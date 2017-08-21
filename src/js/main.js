5e70c8a103be6c69bc9e89b07ee5d751742e3c32
90segundos <a.sotoca.pinilla@gmail.com>
2017-08-21 11:10:21 +0200

    nav button

---
+++ src/js/main.js
@@ -0,0 +1,63 @@
+/* ---------------- [ variables ] ---------------- */
+
+var transition_time = 300;
+
+/* ---------------- [ functions ] ---------------- */
+
+function activate(element){
+  if(!element){
+    return false;
+  }else{
+    $(element).addClass('active');
+  }
+}
+
+function deactivate(element){
+  if(!element){
+    return false;
+  }else{
+    $(element).filter('.active').removeClass('active');
+  }
+}
+
+function toggleActive(element){
+  if(!element){
+    return false;
+  }else if($(element).hasClass('active')){
+    deactivate(element);
+  }else{
+    activate(element);
+  }
+}
+
+function buttonNavInit(){
+  var blocked = false;
+  $('.c-nav-button').on('click',function(event){
+    event.preventDefault();
+    if(!blocked){
+      blocked = true;
+      toggleActive(this);
+      setTimeout(function(){
+        blocked = false;
+      },transition_time);
+    }else{
+      console.log('blocked');
+    }
+  });
+}
+
+function activateOnClick(trigger, target){
+  // home-menu moves up
+  $(trigger).on('click', function(event){
+    event.preventDefault();
+    activate(target);
+  });
+
+  // display ajax content
+}
+
+/* ---------------- [ init ] ---------------- */
+
+$(document).ready(function(){
+  buttonNavInit();
+});
