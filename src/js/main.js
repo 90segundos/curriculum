c77318d2c27ac2c39454b54a4fa18812d917cc2c
90segundos <a.sotoca.pinilla@gmail.com>
2017-08-22 07:31:15 +0200

    circle graphic canvas
    first test

--- src/js/main.js
+++ src/js/main.js
@@ -56,8 +56,54 @@ function activateOnClick(trigger, target){
   // display ajax content
 }
 
+function graph_1(){
+
+  var values = [
+    70,
+    65,
+    60,
+    70,
+    70,
+    60
+  ]
+
+
+  var canvas = document.getElementById("myCanvas");
+
+  var grey_path = [];
+  var i = 0;
+  for (i; i<6; i++) {
+    console.log(i);
+    grey_path[i] = canvas.getContext("2d");
+    grey_path[i].beginPath();
+    grey_path[i].arc(125,125,15*(i+2),0,2*Math.PI);
+    grey_path[i].lineWidth=1;
+    grey_path[i].setLineDash([4,4]);
+    grey_path[i].lineColor="#333333";
+    grey_path[i].strokeStyle="#333333";
+    grey_path[i].lineCap="round";
+    grey_path[i].stroke();
+  }
+
+  var color_path = [];
+  var i = 0;
+  for (i; i<6; i++) {
+    console.log(i);
+    color_path[i] = canvas.getContext("2d");
+    color_path[i].beginPath();
+    color_path[i].arc(125,125,15*(i+2),0,(values[i]/100)*(2*Math.PI));
+    color_path[i].lineWidth=3;
+    color_path[i].setLineDash([]);
+    color_path[i].lineColor="#b68845";
+    color_path[i].strokeStyle="#b68845";
+    color_path[i].lineCap="round";
+    color_path[i].stroke();
+  }
+}
+
 /* ---------------- [ init ] ---------------- */
 
 $(document).ready(function(){
   buttonNavInit();
+  graph_1();
 });
